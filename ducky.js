5476ac7eadb8b0ca86ed42c1f2e235e9b431673f
mfrancois3k <41052077+mfrancois3k@users.noreply.github.com>
2021-11-12 10:14:12 -0500

    Update duck.js

--- ducky.js
+++ ducky.js
@@ -3,10 +3,61 @@ window.onload = function() {
 
   console.log(body);
 
+
+
+  const duck = document.createElement('div');
+  duck.className = 'duck';
+  duck.style.top = Math.random() * window.innerWidth + 'px';
+  duck.style.left = Math.random() * window.innerHeight + 'px';
+  document.body.appendChild(duck);
+  setTimeout(function() { duck.remove() }, 1)
+
+  function createDuck() {
+      const duck = document.createElement('div');
+      duck.className = 'duck';
+      duck.style.top = Math.random() * window.innerWidth + 'px';
+      duck.style.left = Math.random() * window.innerHeight + 'px';
+      document.body.appendChild(duck);
+      let flapper = setInterval(function flapDuck() {
+          duck.classList.toggle('flap');
+      }, 250);
+
+      function moveDuck() {
+          let top = Math.random() * window.innerWidth;
+          let left = Math.random() * window.innerHeight;
+
+          duck.style.top = top + 'px';
+          duck.style.left = left + 'px';
+
+          function addShot() {
+              duck.classList.add('shot');
+              setTimeout(function() { duck.remove(); checkForWinner() }, 1000)
+          }
+
+          duck.addEventListener('click', addShot);
+      }
+
+      moveDuck()
+      let mover = setInterval(moveDuck, 1000);
+      return duck;
+  };
+
+  for (let i = 0; i < 5; i++) {
+      createDuck()
+  }
+
+  function checkForWinner() {
+  if (document.getElementsByClassName('duck').length == 0) {
+      alert('You win!')
+    }
+  }
   // 1. Create a <div> with the class "duck" and add it to the body.  Do this step by step
   // ( 1. create the element
+
   //   2. add a class to the element
+ 
   //   3. append the element to the body )
+
 
   // 2. Next, use setInterval to toggle the "flap" class on the duck every 250 ms (1/4 second)
   // https://www.w3schools.com/jsref/met_win_setinterval.asp
@@ -16,8 +67,27 @@ window.onload = function() {
   // "top" and "left" CSS properties.
   // HINT: Use Math.random() * window.innerWidth    for "left"
   //       And Math.random() * window.innerHeight   for "top"
+  // function moveDuck(duck) {
+  //   create.style.top = Math.random() * window.innerHeight + 'px';
+  //   create.style.left = Math.random() * window.innerWidth + 'px';
+  // }
 
   // 4. Try making the duck move to a different location every second (what did we use to do this several lines up??)
+// function shotDown() {
+//   create.classList.add('shot');
+//   setTimeout(function () {
+//     create.remove();
+//     checkForWinner();
+//   }
+// }
+//   function moveDuckEverySecond(duck) {    
+//     let duckMove = setInterval(function () {
+//       moveDuck(duck);
+//     }, 1000);
+
+//   setInterval(function() {
+//     moveDuck(create);
+//   }, 1000);
 
   // 5. Congratulations! Move on to part 2!
 
@@ -27,33 +97,68 @@ window.onload = function() {
   //    a "function" called createDuck() that does everything in 1-4
   //    and "returns" the duck object
 
+
   // 7. Now, let's create lots of ducks!  Use a "for" loop to create 5 ducks
   //    using our fancy new createDuck() function
+
 
   // 8. The ducks are overlapping.  Modify createDuck so each time
   //     it creates a duck, it appears in a random location
   // HINT: You may want to create a `randomPosition()` function that you can use
   //       to set the ducks' initial locations and in your `moveDuck()` function;
 
-  // 9. Keep going! Move onto part 3!
+//   function randomPosition(duck) {
+//     duck.style.top = Math.random() * window.innerHeight + 'px';
+//     duck.style.left = Math.random() * window.innerWidth + 'px';
+//   }
+   
+//   // 9. Keep going! Move onto part 3!
 
-  // --------------------------- PART 3 ------------------------------------
+//   setInterval(function () {
+//     let rand = Math.floor(Math.random() * 100);
+//     if (rand < 50) {
+//       duck.style.transform = 'scale(-1,1)';
+//       moveDuck(duck);
+//     } else {
+//       duck.style.transform = 'scale(1,1)';
+//       moveDuck(duck);
+//     }
+//   }, 1000);
 
-  // 11. BOOM. Attach a "click" handler that adds the "shot" class to
-  //     the duck when you click on it!
+//   randomPosition(duck);
+//   // --------------------------- PART 3 ------------------------------------
 
-  // 12. After a duck has been clicked on, remove it from the DOM after
-  //     a short delay (1 second) Hint Hint...use setTimeout
-  //     as for removing the element check out https://dzone.com/articles/removing-element-plain
+//   // 11. BOOM. Attach a "click" handler that adds the "shot" class to
+//   //     the duck when you click on it!
 
-  // 13. Create a new function named checkForWinner() that reads the DOM
-  //     to see if there are any ducks left. (How can we check the DOM for more than one element?, and how can we see how many elements we get back) If not, alert "YOU WIN!"
+//   // 12. After a duck has been clicked on, remove it from the DOM after
+//   //     a short delay (1 second) Hint Hint...use setTimeout
+//   //     as for removing the element check out https://dzone.com/articles/removing-element-plain
+  
 
-  // 14. BONUS: The ducks are moving pretty erratically, can you think
-  //     of a way to adjust the ducks speed based on how far needs to move?
+//   // 13. Create a new function named checkForWinner() that reads the DOM
+//   //     to see if there are any ducks left. (How can we check the DOM for more than one element?, and how can we see how many elements we get back) If not, alert "YOU WIN!"
 
-  // 15. BONUS: Add the "left" and "right" class to the duck based on the
-  //     direction the duck is flying and change the way the duck is facing
+//   // function checkForWinner() {
+//   //   if (document.querySelectorAll('.duck').length === 0) {
+//   //     alert('YOU WIN!');
+//   //   }
+//   // // 14. BONUS: The ducks are moving pretty erratically, can you think
+//   //     of a way to adjust the ducks speed based on how far needs to move?
 
-  // Done, you have accomplish another level of skill
-};
+
+//   // 15. BONUS: Add the "left" and "right" class to the duck based on the
+//   //     direction the duck is flying and change the way the duck is facing
+
+//   // Done, you have accomplish another level of skill
+//   duckShot = () => {
+// 		create.classList.add('shot');
+// 		setTimeout(function() {
+// 			create.remove();
+
+// 			checkForWinner();
+// 		}, 1000);
+// 	};
+
+// 	create.addEventListener('click', duckShot);
+// };
